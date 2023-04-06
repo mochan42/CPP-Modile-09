6ed9ecc65b9a366424164526c3ddaf37ce1c8c82
mochan42 <mochan@students.42wolfsburg.de>
2023-04-06 14:22:12 +0200

    setp ex00

---
+++ ex00/inc/BitcoinExchange.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moninechan <moninechan@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/06 11:50:47 by moninechan        #+#    #+#             */
+/*   Updated: 2023/04/06 12:12:12 by moninechan       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+#endif
---
+++ ex00/src/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moninechan <moninechan@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/06 11:50:45 by moninechan        #+#    #+#             */
+/*   Updated: 2023/04/06 12:12:38 by moninechan       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char **av)
+{
+    (void) ac;
+    (void) av;
+
+    ifstream btcDatabase
+    std::cout << "hello\n";
+    return (0);   
+}
