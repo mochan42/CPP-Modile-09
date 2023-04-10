c5529f826001ed9c5b33b3846096bb2b2feae625
mochan42 <mochan@students.42wolfsburg.de>
2023-04-10 10:58:05 +0200

    finalize tests

--- ex02/testPmergeMe.sh
+++ ex02/testPmergeMe.sh
@@ -1,15 +1,16 @@
 echo "=== COMPILING ==================================="
-#make -s re
+make -s re
 echo -e
 echo "=== TESTS ======================================="
 echo "1st test : ./PmergeMe 3 5 9 7 4"
 ./PmergeMe 3 5 9 7 4
 echo -e
 echo "2nd test : ./PmergeMe \"-1\" \"2\" "
+echo "Error expected due to negative integer"
 ./PmergeMe -1 2
 
 echo -e
-echo "3rd test : ./PmergeMe `jot -r 3000 1 100000 | tr '\n' ' '`"
+#echo "3rd test : ./PmergeMe `jot -r 3000 1 100000 | tr '\n' ' '`"
 #./PmergeMe `jot -r 3000 1 2147483647 | tr '\n' ' '` > output
 
 echo -e
